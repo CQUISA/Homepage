6855f9f02ce697069aa423c1e8c906c33ed1e8bc
Yunen <hackeryunen@gmail.com>
2023-05-25 20:11:00 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'spa',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: 'CQUISA 重庆大学信息安全协会',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+    transpile: [/^element-ui/],
+  }
+}
---
+++ pages/glory.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="content">
+		<h3>获奖情况</h3>
+		<div class="glory">
+			<span>2020第十二届全国大学生信息安全大赛作品赛全国二等奖</span>
+      <span>2019重庆市教育系统网络安全攻防赛竞赛团体一等奖</span>
+      <span>2019第十一届全国大学生信息安全大赛技能赛西南赛区三等奖</span>
+      <span>2019第十一届全国大学生信息安全大赛技能赛全国三等奖</span>
+      <span>2018湖湘杯国家级优秀奖</span>
+      <span>2017第十届全国大学生信息安全大赛创新实践能力赛优秀奖</span>
+      <span>2017第十届全国大学生信息安全大赛作品赛全国二等奖</span>
+      <span>2017重庆市”绿盟杯“网络空间安全竞赛决赛组织奖</span>
+      <span>2017重庆市”绿盟杯“网络空间安全竞赛决赛优胜奖</span>
+			<span>etc..</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style>
+  .glory span{
+		display: block;
+	}
+</style>
